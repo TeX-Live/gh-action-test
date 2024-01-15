dd75ae6ab84331a312de65828908792b2f602173
Norbert Preining <norbert@preining.info>
2024-01-16 08:17:39 +0900

    Add TL workflow

---
+++ .github/scripts/build-tl.sh
@@ -0,0 +1,21 @@
+#!/bin/sh -l
+
+arch="$1"
+echo "Building TL for arch = $arch"
+
+destdir="$2"
+
+# for CentOS we need to activate gcc-9
+if [ -f /opt/rh/devtoolset-9/enable ]
+then
+  # we cannot call scl enable devtoolset-9 here since we need
+  # the settings in the running shell
+  . /opt/rh/devtoolset-9/enable
+fi
+
+touch texlive-bin-$arch.tar.gz
+if [ -n "$destdir" ] ; then
+  mv texlive-bin-$arch.tar.gz "$destdir"
+  ls "$destdir"
+fi
+
